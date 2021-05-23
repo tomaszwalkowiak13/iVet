7dfa665f527c32a6fbb326e5b16bd4911a6d7776
tomaszwalkowiak89 <tomash1309@gmail.com>
2021-05-23 21:58:17 +0200

    Add the photo model

---
+++ app/Photo.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Photo extends Model
+{
+
+    public function photoable()
+    {
+        return $this->morphTo();
+    }
+
+    public function getPathAttribute($value)
+    {
+        return asset("storage/{$value}");
+    }
+
+    public function getStoragepathAttribute()
+    {
+        return $this->original['path'];
+    }
+}
